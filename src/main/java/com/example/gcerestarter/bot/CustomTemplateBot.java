457afd989eea4272529e9e45e3a9e07eac966eca
peterpeter250410 <q779966123@gmail.com>
2025-08-28 22:34:44 +0700

    1.

--- src/main/java/com/example/gcerestarter/bot/CustomTemplateBot.java
+++ src/main/java/com/example/gcerestarter/bot/CustomTemplateBot.java
@@ -218,7 +218,8 @@ public class CustomTemplateBot extends TelegramLongPollingBot {
 
     public static String replaceValue(String name, String value) {
         if (name.contains("cpu异常告警")) {
-            return new BigDecimal(value).multiply(new BigDecimal(100)) + "%";
+            String values = new BigDecimal(value).multiply(new BigDecimal(100)) + "%";
+            return String.format("%." + 2 + "f MB", values);
         } else if (name.contains("流量异常告警")) {
             return ByteUnitConverter.convertBytes(new BigDecimal(value).longValue(), 2);
         }
